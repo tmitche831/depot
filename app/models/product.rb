b5f3110348ca76e675311addf13fe6b71d894fcf
Thomas Mitchell <tmitche831@gmail.com>
2014-11-03 14:47:41 +1100

    unit tests completed for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+	validates :title, uniqueness: true
+	validates :title, :description, :image_url, presence: true
+
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
