dedb5774788f972c5e43af7a2f7e366c408895ea
Thomas Mitchell <tmitche831@gmail.com>
2014-11-05 13:19:07 +1100

    smart shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,7 +20,7 @@ class Product < ActiveRecord::Base
 
 	private
 
-		def ensure_not_reference_by_any_line_item
+		def ensure_not_referenced_by_any_line_item
 			if line_items.empty?
 				return true
 			else
